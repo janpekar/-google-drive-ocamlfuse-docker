c77fb4408d7a3224d86b3598c82805865c2ac2ba
Jan Pekar <jan.pekar@imatic.cz>
2021-11-10 12:22:42 +0100

    Lazy umount and working dir improvements

--- run.sh
+++ run.sh
@@ -8,9 +8,10 @@ ENV_VARS+=" --env=XAUTHORITY=${XAUTH}"
 ENV_VARS+=" --env=UID=$(id -u)"
 VOLUMES+=" --volume=${XSOCK}:${XSOCK}"
 VOLUMES+=" --volume=${XAUTH}:${XAUTH}"
+DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 
 #Umount fuse disk on docker exit
-trap "fusermount -u $(pwd)/volumes/${USER}/googledrive" EXIT
+trap "fusermount -z -u ${DIR}/volumes/${USER}/googledrive" EXIT
 
 #Allow to access X11
 xauth nlist "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "$XAUTH" nmerge -
@@ -18,6 +19,6 @@ xauth nlist "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "$XAUTH" nmerge -
 docker run -i -t $VOLUMES $ENV_VARS \
            --device /dev/fuse \
            --cap-add SYS_ADMIN \
-           --mount type=bind,source=$(pwd)/volumes/$USER,target=/home/$USER,bind-propagation=rshared \
+           --mount type=bind,source=${DIR}/volumes/$USER,target=/home/$USER,bind-propagation=rshared \
            google-drive-ocamlfuse:latest
 
